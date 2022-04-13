1d760c64684176d3fa5a54916c5ea2482998e927
MehdiKBi <mehdi.khosravani@corplife.at>
2022-04-13 09:41:08 +0300

    try try try

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          Learn React and hosting on git today
+          Learn React and hosting on git today, so what?
         </p>
       
       </header>
