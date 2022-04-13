4dcb01b31d6e1d451e15bc6cd132621d965fa86f
MehdiKBi <mehdi.khosravani@corplife.at>
2022-04-13 10:37:52 +0300

    some minor changes

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          Learn React and hosting on git today, so what?
+          Learn React and hosting on git today, so what Mehdi?
         </p>
       
       </header>
