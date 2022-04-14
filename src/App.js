3927d476c47e1800fda456b465e1d6ec265ddb23
MehdiKBi <mehdi.khosravani@corplife.at>
2022-04-15 00:47:45 +0300

    toss new branch

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+
+
 function App() {
+
+  const [result, setResult] = useState(null);
+  const tossMyCoin = () => {
+    let tossCoin = Math.floor(Math.random() * 2);
+    tossCoin === 1 ? setResult('TAILS') : setResult('HEADS')
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          Learn React and hosting on git today, so what Mehdi?
-        </p>
-      
-      </header>
+      <button type="button" onClick={tossMyCoin} data-bs-toggle="modal" data-bs-target="#exampleModal"
+        className='toss_btn'>TOSS</button>
+
+      <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-body">
+              <p style={{ color: 'white' }}>{result}</p>
+            </div>
+            <div className="modal-footer">
+              <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
     </div>
   );
 }
