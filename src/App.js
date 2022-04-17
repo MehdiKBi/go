0f50c26a3ebd8e2390b92db108484a2c7e819800
MehdiKBi <mehdi.khosravani@corplife.at>
2022-04-17 15:47:16 +0300

    new update on coin app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import {useState } from 'react';
+
 import './App.css';
 
 
@@ -6,24 +7,32 @@ import './App.css';
 function App() {
 
   const [result, setResult] = useState(null);
+  const [rotat, setRotate] = useState('image-of-coin-rotate');
+  const [tgl, setTgl] = useState(false);
+
+
   const tossMyCoin = () => {
-    let tossCoin = Math.floor(Math.random() * 2);
-    tossCoin === 1 ? setResult('TAILS') : setResult('HEADS')
+    setTgl(true);
+    setTimeout(()=>{
+      let tossCoin = Math.floor(Math.random() * 2);
+      tossCoin === 1 ? setResult('TAILS') : setResult('HEADS')
+      console.log(result)
+      setTgl(false);
+    },1000)
   }
+
+  
 
   return (
     <div className="App">
-      <button type="button" onClick={tossMyCoin} data-bs-toggle="modal" data-bs-target="#exampleModal"
-        className='toss_btn'>TOSS</button>
+      <button className={tgl ? 'image-of-coin-rotate' : '' } id='image-of-coin' type="button" onClick={tossMyCoin} data-bs-toggle="modal" data-bs-target="#exampleModal"></button>
 
-      <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div className="modal fade" id="exampleModal" tabIndex="2" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div className="modal-dialog">
           <div className="modal-content">
-            <div className="modal-body">
-              <p style={{ color: 'white' }}>{result}</p>
-            </div>
-            <div className="modal-footer">
-              <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            <div className="modal-body head-tail-body">
+              {result === 'TAILS' && <p className="result-img" id='tail-image' ></p>}
+              {result === 'HEADS' && <p className="result-img" id='heads-image' ></p>}
             </div>
           </div>
         </div>
